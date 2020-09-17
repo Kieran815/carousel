6d1a6ada1d227d64b848f6edbfd27b1805599f08
Kieran815 <kieran.milligan@gmail.com>
2020-09-17 11:31:57 -0500

    running pre-deploy and deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import Fremen from './images/fremen.jpg';
+import Sandworms from './images/Sandworms.jpg';
+import Paul from './images/Paul.jpg';
+import Leto from './images/GodEmperorOfDune.jpg';
+import Spice from './images/SpiceMelange.jpg';
 
+let styles = {
+    margin: 'auto',
+    width: '500px',
+    maxHeight: 'auto',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  };
+
+let imgStyle = {
+  width: '500px',
+  height: 'auto'
+}
+  
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={styles}>
+      <h1 style={{textAlign: 'center'}}>Faces of Dune</h1>
+      <Carousel infiniteLoop dynamicHeight autoPlay>
+        <div>
+          <img src={Fremen} alt="The Fremen" />
+          <p className="legend">The Fremen</p>
+        </div>
+        <div>
+          <img src={Paul} alt="Paul Atreides"/>
+          <p className="legend">Paul</p>
+        </div>
+        <div>
+          <img src={Sandworms} alt="Sandworms"/>
+          <p className="legend">Sandworms</p>
+        </div>
+        <div>
+          <img src={Leto} alt="Leto Atreides"/>
+          <p className="legend">Leto Atreides, God Emperor of Dune</p>
+        </div>
+        <div>
+          <img src={Spice} alt="Spice Melange"/>
+          <p className="legend">The Spice Must Flow</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
