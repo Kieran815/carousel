19731f93b28d28bce10f0d03dee1da28a3c8a47e
Kieran815 <kieran.milligan@gmail.com>
2020-09-17 11:35:00 -0500

    Updates

---
+++ precache-manifest.d5e76ec3b915b9b5973171358a8f6ade.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eaa3daf9f1220392d675ffbb67e3e89",
+    "url": "/carousel/index.html"
+  },
+  {
+    "revision": "d336d876e8026315f494",
+    "url": "/carousel/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "5f405e1db0366b1066aa",
+    "url": "/carousel/static/css/main.6413bfa0.chunk.css"
+  },
+  {
+    "revision": "d336d876e8026315f494",
+    "url": "/carousel/static/js/2.a87b535f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/carousel/static/js/2.a87b535f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f405e1db0366b1066aa",
+    "url": "/carousel/static/js/main.5331dcc3.chunk.js"
+  },
+  {
+    "revision": "45fcbf2647b7b5eeb003",
+    "url": "/carousel/static/js/runtime-main.e91812ac.js"
+  },
+  {
+    "revision": "cce415c07774c901c8324f9aed70968d",
+    "url": "/carousel/static/media/GodEmperorOfDune.cce415c0.jpg"
+  },
+  {
+    "revision": "9c8c98bde3b0cfa7f1af757490f49b80",
+    "url": "/carousel/static/media/Paul.9c8c98bd.jpg"
+  },
+  {
+    "revision": "fa4c261c44c06614bfe3d47ff4789c7a",
+    "url": "/carousel/static/media/Sandworms.fa4c261c.jpg"
+  },
+  {
+    "revision": "4297de1760d57f54d48ff7a98a17194f",
+    "url": "/carousel/static/media/SpiceMelange.4297de17.jpg"
+  },
+  {
+    "revision": "bf090d736eefa9cf5b0362eb34198a60",
+    "url": "/carousel/static/media/fremen.bf090d73.jpg"
+  },
+  {
+    "revision": "3d339e93674c3ee5721d9ca54da88610",
+    "url": "/carousel/static/media/sand.3d339e93.jpg"
+  }
+]);
